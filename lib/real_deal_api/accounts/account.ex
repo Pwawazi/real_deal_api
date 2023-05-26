76e3b1816978c694c4726a62aee59d62bf62f161
Wawazi <pwawazi@kenet.or.ke>
2023-05-26 16:29:15 +0300

    Initial commit

---
+++ lib/real_deal_api/accounts/account.ex
@@ -0,0 +1,64 @@
+defmodule RealDealApi.Accounts.Account do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+  schema "accounts" do
+    field :email, :string
+    field :hashed_password, :string
+
+    has_one(:user, RealDealAPI.Users.User)
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(account, attrs) do
+    account
+    |> cast(attrs, [:email, :hashed_password])
+    |> validate_required([:email, :hashed_password])
+    |> validate_format(:email, ~r/^[^\s]+@[^\s]+$/, message: "Email must contain the '@' symbol and contain no spaces")
+    |> validate_length(:email, max: 160)
+    |> unique_constraint(:email)
+    |> put_password_hash()
+  end
+
+  defp put_password_hash(%Ecto.Changeset{valid?: true, changes: %{hashed_password: hashed_password}} = changeset) do
+    change(changeset, hashed_password: Bcrypt.hash_pwd_salt(hashed_password))
+  end
+  defmodule RealDealApi.Accounts.Account do
+    use Ecto.Schema
+    import Ecto.Changeset
+
+    @primary_key {:id, :binary_id, autogenerate: true}
+    @foreign_key_type :binary_id
+    schema "accounts" do
+      field :email, :string
+      field :hashed_password, :string
+      
+      has_one :user, RealDealApi.Users.User
+
+      timestamps()
+    end
+
+    @doc false
+    def changeset(account, attrs) do
+      account
+      |> cast(attrs, [:email, :hashed_password])
+      |> validate_required([:email, :hashed_password])
+      |> validate_format(:email, ~r/^[^\s]+@[^\s]+$/, message: "must have the @ sign and no spaces")
+      |> validate_length(:email, max: 160)
+      |> unique_constraint(:email)
+      |> put_password_hash()
+    end
+
+    defp put_password_hash(%Ecto.Changeset{valid?: true, changes: %{hashed_password: hashed_password}} = changeset) do
+      change(changeset, hashed_password: Bcrypt.hash_pwd_salt(hashed_password))
+    end
+
+    defp put_password_hash(changeset), do: changeset
+  end
+
+  defp put_password_hash(changeset), do: changeset
+end
